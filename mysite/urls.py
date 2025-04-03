a69345a001573b75340a5f7ac7ca949b06be2614
nirupamjm <nirupamjm@gmail.com>
2025-04-04 02:17:02 +1100

    test

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,10 +3,17 @@ from django.urls import path, include
 from . import views
 from main import views as main_views 
 from register import views as v
+from django.contrib.auth.views import LogoutView
+
+class CustomLogoutView(LogoutView):
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("register/", v.register, name="register"), 
     path('home', main_views.home, name='home'), 
-    path('', main_views.home, name='home'), 
+    path('', main_views.home, name='home'),
+    path('', include("django.contrib.auth.urls")),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
