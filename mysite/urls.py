100bf0ac52cb267da32c1d1fc1267e3b853030be
nirupamjm <nirupamjm@gmail.com>
2025-04-03 14:52:48 +1100

    test

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from main import views as main_views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
-    path('register/', views.register, name='register'),
+    path('', main_views.home, name='home'), 
 ]
