38147e8c358584cf38979c2fbe1a32e03a7a4e32
nirupamjm <68326831+nirupamjm@users.noreply.github.com>
2025-04-04 02:55:33 +1100

    Login (#4)
    * test
    
    * test
    
    * test
    
    * test

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,10 +3,20 @@ from django.urls import path, include
 from . import views
 from main import views as main_views 
 from register import views as v
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.decorators import login_required
+
+class CustomLogoutView(LogoutView):
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("register/", v.register, name="register"), 
     path('home', main_views.home, name='home'), 
-    path('', main_views.home, name='home'), 
+    path('', main_views.home, name='home'),
+    path('', include("django.contrib.auth.urls")),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('profile/', login_required(main_views.profile), name='profile'),
+    path('login/', include("django.contrib.auth.urls")),  # Ensure this is included
 ]
